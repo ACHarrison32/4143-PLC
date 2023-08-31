c28d7f055e12f85a860435cf41450bd1bdbf6e17
ACHarrison32 <ACHarrison1123@gmail.com>
2023-08-31 10:37:20 -0500

    More Notes

--- Lecture Notes/Lecture02.go
+++ Lecture Notes/Lecture02.go
@@ -64,28 +64,87 @@
 
 // ======================================================
 
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	// Basic for loop
-	for i := 1; i <= 20; i++ {
-		fmt.Print(i, " ")
-	}
-	fmt.Println()
+// func main() {
+// 	// Basic for loop
+// 	for i := 1; i <= 20; i++ {
+// 		fmt.Print(i, " ")
+// 	}
+// 	fmt.Println()
 
-	// Looping through an array/slice
-	//numbers := []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
-	numbers2 := [5]int{0}
+// 	// Looping through an array/slice
+// 	//numbers := []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
+// 	numbers2 := [5]int{0}
 
-	numbers2[0] = 99
-	numbers2[1] = 80
-	numbers2[2] = 70
-	for _, num := range numbers2 {
-		fmt.Print(num, " ")
-	}
-	fmt.Println()
-}
+// 	numbers2[0] = 99
+// 	numbers2[1] = 80
+// 	numbers2[2] = 70
+// 	for _, num := range numbers2 {
+// 		fmt.Print(num, " ")
+// 	}
+// 	fmt.Println()
+// }
 
 // ======================================================
+
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+
+	var s []string
+	fmt.Println("uninit:", s, s == nil, len(s) == 0)
+
+	s = make([]string, 3)
+	fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
+
+	fmt.Println("len:", len(s))
+
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("apd:", s)
+
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("cpy:", c)
+
+	l := s[2:5]
+	fmt.Println("sl1:", l)
+
+	l = s[:5]
+	fmt.Println("sl2:", l)
+
+	l = s[2:]
+	fmt.Println("sl3:", l)
+
+	t := []string{"g", "h", "i"}
+	fmt.Println("dcl:", t)
+
+	t2 := []string{"g", "h", "i"}
+	if slices.Equal(t, t2) {
+		fmt.Println("t == t2")
+	}
+
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+}
