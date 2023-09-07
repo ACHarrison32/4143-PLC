fb0745e2f684dd0cd8cf605563c4929f2a705689
ACHarrison32 <ACHarrison1123@gmail.com>
2023-09-07 10:26:56 -0500

    Hello

--- Lecture Notes/Lecture04.go
+++ Lecture Notes/Lecture04.go
@@ -5,3 +5,22 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 }
+
+/* This is a simple example of how to use a global variable in Python
+A = 10
+
+def func(){
+	global A
+}
+*/
+
+/*
+Arrays: Fixed size sequence of elements of the same type  [size]type
+Slices: Dynamic arrays with flexible size    []type
+Maps: Key-value pairs (association arrays)   map[keyType]valueType
+Structs: Composite data type that groups fields with different data types
+*/
+
+/*
+
+ */
