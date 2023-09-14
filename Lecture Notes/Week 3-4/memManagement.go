cb4b61304c49a0a71238750e2ca237ebabff0444
ACHarrison32 <ACHarrison1123@gmail.com>
2023-09-13 23:44:06 -0500

    mem management notes

--- Lecture Notes/Week 3-4/memManagement.go
+++ Lecture Notes/Week 3-4/memManagement.go
@@ -1,1 +1,25 @@
-.
+/*
+
+Memory allocation and Deallocation happens automatically
+
+new()
+Allocate memort but no INIT
+you will get a memory address
+zeroed storage
+
+make()
+Allocate memory and INIT
+you will get a memory address
+non-zeroed storage
+
+Garbage collection happens automatically
+Out of scope or nil
+
+https://pkg.go.dev/runtime => package runtime
+
+The GOGC variable sets the initial garbage collection target percentage. A collection is
+triggered when the ratio of freshly allocated data to live data remaining after the previous
+collection reaches this percentage.
+
+*/
+
